853c108c3011fa798f416bb381136d996ecd998b
scarf005 <greenscarf005@gmail.com>
2022-04-14 09:12:16 +0900

    test: :white_check_mark: add test module

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,16 +5,24 @@
 using std::cout;
 
 int main() {
+  test::header("int");
   {
     int a = 42, b = 24, c = 20, d = 20;
     test_all(a, b, "int");
     test_all(c, d, "same int");
   }
   {
+    const int a = 42, b = 24, c = 20, d = 20;
+    test_cmp(a, b, "const int");
+    test_cmp(c, d, "const same int");
+  }
+  test::header("double");
+  {
     double a = 42.42, b = 24.24, c = 20.20, d = 20.20;
     test_all(a, b, "double");
     test_all(c, d, "same double");
   }
+  test::header("std::string");
   {
     std::string a = "42", b = "24", c = "20", d = "20";
     std::string e = "abc", f = "def", g = "bye", h = "bye";
@@ -23,6 +31,7 @@ int main() {
     test_all(e, f, "string");
     test_all(g, h, "same string");
   }
+  test::header("custom object");
   {
     MyInt a(42), b(24), c(20), d(20);
     test_all(a, b, "MyInt");
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include "util.hpp"
 
 using std::cout;
 using std::string;
@@ -29,34 +30,40 @@ T& min(T& a, T& b) {
 template <typename T>
 string pair_to_string(T& a, T& b) {
   std::ostringstream ss;
-  ss << "[" << a << "," << b << "]";
+  ss << MAG "(" BBLU << a << BLU ", " BBLU << b << MAG ")";
   return ss.str();
 }
 
 template <typename T>
 void test_swap(T& a, T& b) {
-  cout << "swap" << pair_to_string(a, b) << "=>";
+  cout << YEL "swap" << pair_to_string(a, b) << RED " -> ";
   swap(a, b);
   cout << pair_to_string(a, b) << "\n";
 }
 
 template <typename T>
 void test_max(T& a, T& b) {
-  cout << "max" << pair_to_string(a, b) << "=>";
+  cout << YEL "max" << pair_to_string(a, b) << RED " -> " BBLU;
   cout << max(a, b) << "\n";
 }
 
 template <typename T>
 void test_min(T& a, T& b) {
-  cout << "min" << pair_to_string(a, b) << "=>";
+  cout << YEL "min" << pair_to_string(a, b) << RED " -> " BBLU;
   cout << min(a, b) << "\n";
 }
 
 template <typename T>
-void test_all(T& a, T& b, const string& type) {
-  cout << "\n<Testing for " << type << ">\n";
-  test_swap(a, b);
+void test_cmp(T& a, T& b, const string& type) {
+  test::subject(type);
   test_max(a, b);
   test_min(a, b);
 }
+
+template <typename T>
+void test_all(T& a, T& b, const string& type) {
+  test_cmp(a, b, type);
+  test_swap(a, b);
+}
+
 #endif  // __WHATEVER_H__
